d3eda4b43056539c3a6aa18d7d731d5dba0b2615
Juan Camilo Villafane <juangaav@gmail.com>
2024-11-26 18:04:39 -0500

    Update database.js

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,5 +1,5 @@
 const parse = require('pg-connection-string').parse;
-const config = parse(process.env.DATABASE_URL);
+const config = parse('postgres://uc66rs321dnv93:p0e41dfc7a2faacabfe2cc35acf930a9e429e50e9ab753f6dc33e4f8cca7c573e@cf980tnnkgv1bp.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/dcpnl4ua4lp7c');
 module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
