86891d73fa2ab7ac625a1205e007697be2dcdbd9
Juan Camilo Villafane <juangaav@gmail.com>
2024-11-26 19:56:46 -0500

    Cambios en keys

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -2,8 +2,8 @@
 
 export default ({ env }) => ({
     proxy: true,
-    url: env('MY_HEROKU_URL'), // Sets the public URL of the application.
+    url: env('https://elearning-strapi-f0a587e9a326.herokuapp.com/'), // Sets the public URL of the application.
     app: { 
-         keys: env.array('APP_KEYS')
+        keys: env.array("APP_KEYS", ["2HE85F3RgFpL41N6xC/veg==", "8MIv77QcCOmSpnnrm6pjmQ==", "zv9uJWsJvIs2jQlNNAHU2g==","Vr46HUc79fNrEOpKBBbbkw=="]),
          },
    });
